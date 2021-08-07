9ed6a2fb279f5cad2648153b8914bc129de56b7e
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-08-07 22:03:54 +0530

    [akhila]added:start the game when position is 0

---
+++ snakeandladder.js
@@ -0,0 +1,8 @@
+//start the game position is 0
+let readline = require("readline-sync");
+let n = readline.question("Enter the  position: ");
+function position(n){
+    if(n == 0)
+    console.log("start the game");
+}
+console.log(position(n));
