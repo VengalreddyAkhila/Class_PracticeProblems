90e5f586c72d4039183cff9f52a36d9809272fde
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-08-08 21:29:50 +0530

    [akhila]Refactor:Code modified

--- snakeandladder.js
+++ snakeandladder.js
@@ -5,26 +5,27 @@ class Game{
         this.dice = dice;
     }
 }
-let Player = new Game(0 ,Math.floor(Math.random()*7) );
+
+let Player = new Game(0 ,0);
 console.log(Player) ;
 console.log("start game");
 while (Player.position != 100)
 {
-    let readline = require("readline-sync");
+    Player.dice = Math.floor(Math.random()*7);
     let choice = Math.floor(Math.random()*3);
     switch(choice)
     {
-            case 1:
-                if(Player.position >= 100)
-                console.log("you win");
-                break;
-            case 2:  
+            case 0:  
                 Player.position += Player.dice;
                 console.log("got ladder: " +Player.position);
                 break;
-            case 3:
+            case 1:
                 Player.position -= Player.dice;
                 console.log("snake attack: " +Player.position);
+                if(Player.position <= 0)
+                {
+                Player.position = 0;
+                }
                 break;
             default:
                 console.log("No play");
