064a40967929abf34d3f55e70cd32e0220bbc5f3
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-08-08 10:37:31 +0530

    [akhila]Refactor: snake and ladder options

--- snakeandladder.js
+++ snakeandladder.js
@@ -1,10 +1,40 @@
-class Player{
+class Game{
     constructor(position,dice)
     {
         this.position = position;
         this.dice = dice;
     }
 }
-let game = new Player(0 ,Math.floor(Math.random()*7) );
-console.log(game ) ;
+let Player = new Game(0 ,Math.floor(Math.random()*7) );
+console.log(Player) ;
 console.log("start game");
+let readline = require("readline-sync");
+let number = readline.question("Enter the choice: ");
+let choice = parseInt(number);
+switch(choice)
+{
+    case 1:
+       Player.position === 0
+       console.log("no play");
+    break;
+    case 2:
+        Player.position += Player.dice;
+            if(Player.position == 0)
+            {
+                console.log("you lose ");
+            }
+            else
+            {
+            console.log("got ladder: " +Player.position);
+            }
+     break;
+     case 3:
+         Player.position -= Player.dice;
+         console.log("snake attack: " +Player.position);
+         break;
+     default:
+         console.log("invalid choice");
+         break;
+}
+
+        
